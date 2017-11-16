894d5cacfb4c38feee3115dc42babb7739fd8a91
kcrowder13 <kara.crowder@gmail.com>
2017-11-16 16:38:10 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/commentDB', {useMongoClient:true});
+mongoose.connect('mongodb://localhost/usersDB', {useMongoClient:true});
 
-var commentSchema = mongoose.Schema({
+var usersSchema = mongoose.Schema({
   Name:String,
-  Comment:String
+  Password:String
 });
 
-var Comment = mongoose.model('Comment',commentSchema);
+var User = mongoose.model('User',usersSchema);
 var db = mongoose.connection;
 db.on('error',console.error.bind(console,'connection error'));
 db.once('open',function(){
@@ -18,34 +18,34 @@ db.once('open',function(){
 
 router.get('/fake',function(req,res,next){
    console.log("fake");
-  var fakelist = [{Name:"jim",Comment:"hi"}];
+  var fakelist = [{Name:"jim",Password:"hi"}];
   res.json(fakelist);
 });
 
-router.get('/comment',function(req,res,next) {
-  console.log("Comment Get");
-  Comment.find(function(err,commentList) {
+router.get('/user',function(req,res,next) {
+  console.log("User Get");
+  User.find(function(err,userList) {
     if(err) return console.error(err);
     else{
-      res.json(commentList);
+      res.json(userList);
     }
   });
 });
 
-router.post('/comment',function(req,res,next){
-  console.log("Comment Post");
+router.post('/user',function(req,res,next){
+  console.log("User Post");
   console.log(req.body);
-  var newcomment = new Comment(req.body);
-  newcomment.save(function(err,post) {
+  var newUser = new User(req.body);
+  newUser.save(function(err,post) {
     if(err) return console.error(err);
     console.log(post);
     res.sendStatus(200);
   });
 });
 
-router.delete('/comment',function(req,res,next) {
-  console.log("delete route");
-  Comment.remove(function(err) {
+router.delete('/user',function(req,res,next) {
+  console.log("delete user");
+  User.remove(function(err) {
     if(err) return console.error(err);
     else {
       console.log("Success");
