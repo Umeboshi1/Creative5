f5235a15501f66bdf8a0b421952509a3e0f48123
kcrowder13 <kara.crowder@gmail.com>
2017-11-16 18:08:46 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -25,9 +25,9 @@ router.get('/fake',function(req,res,next){
 router.get('/user',function(req,res,next) {
   console.log("User Get");
   
-  var obj = JSON.parse(req.body); // this is how you parse a string into JSON 
+//   var obj = JSON.parse(req.body); // this is how you parse a string into JSON 
 //   document.body.innerHTML += obj.hello;
-  console.log(obj);
+//   console.log(obj);
   
   var myCursor = User.find({"Username": "obj.Username"}, {"Password" : "obj.Password" });
 
